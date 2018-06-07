979a1ae98cfdfeaa2b7607f6007bf9fd718fe987
Abhishek Dubey <abhishekdubey331@gmail.com>
2018-06-07 15:24:58 +0530

    New data to scrap

--- helpers.js
+++ helpers.js
@@ -1,13 +1,13 @@
 let axios = require('axios');
 let cheerio = require('cheerio');
-let base_url = 'https://www.goodreads.com/quotes/tag/inspirational';
+let base_url = 'http://www.status77.in/2015/03/cool-whatsapp-status.html';
 
 
 module.exports.getData = function getData(req,res) {
     axios.get(base_url).then((response) => {
         let $ = cheerio.load(response.data);
         let kurs = [];
-        $('.quoteText').each((i, elm) => {
+        $('blockquote').each((i, elm) => {
             kurs.push({
                 quote: $(elm).text().replace(/\n/g, "").trim(),
             });
